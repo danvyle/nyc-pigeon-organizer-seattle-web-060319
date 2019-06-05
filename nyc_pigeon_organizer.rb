2a6ca343ab67fc6ef02c6c7c29ad75d8bc3c4007
Dan-Vy Le <danvyle85@gmail.com>
2019-06-05 12:58:43 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  data[:color].each do |color|
+
+  end
+
+  pigeon_data[:gender].each do |gender, pigeon|
+  end
+
+  pigeon_data[:lives].each do |location, pigeon|
+  end
+  binding.pry
 end
